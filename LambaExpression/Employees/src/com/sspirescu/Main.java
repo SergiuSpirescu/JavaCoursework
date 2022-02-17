9e8bd234a050b591d089d2dd3b379d090bab5448
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-02-17 21:12:44 +0200

    Passing Function object as parameter
    Passing Function object as parameter to a function to get first OR last name of an employee

--- LambaExpression/Employees/src/com/sspirescu/Main.java
+++ LambaExpression/Employees/src/com/sspirescu/Main.java
@@ -33,15 +33,31 @@ public class Main {
         String lastName = getLastName.apply(employees.get(2));
         System.out.println(lastName);
 
+        Function<Employee, String> getFirstName = (Employee employee) -> {
+            return employee.getName().substring(0, employee.getName().indexOf(' '));
+        };
+
+        Random random1 = new Random();
+
+        for (Employee employee : employees) {
+            if(random1.nextBoolean()){
+                System.out.println(getAName(getFirstName, employee));
+            } else {
+                System.out.println(getAName(getLastName, employee));
+            }
+        }
+
+
 //        employees.forEach(employee -> {
 //            String lastName = employee.getName().substring(employee.getName().indexOf(' ') + 1);
 //            System.out.println("Last Name is: " + lastName);
 //        });
 
-
-
     }
 
+    private static String getAName(Function<Employee, String> getName, Employee employee) {
+        return getName.apply(employee);
+    }
     private static void printEmployeesByAge(List<Employee> employees,
                                             String ageText,
                                             Predicate<Employee> ageCondition) {
