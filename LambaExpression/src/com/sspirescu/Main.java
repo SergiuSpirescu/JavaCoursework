973c126b72237c462150eb86356e8084e8fb32dc
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-02-17 20:04:00 +0200

    Still trying to fix .gitignore

--- LambaExpression/src/com/sspirescu/Main.java
+++ LambaExpression/src/com/sspirescu/Main.java
@@ -22,7 +22,7 @@ public class Main {
             System.out.println(employee.getName());
             System.out.println(employee.getAge());
         });
-        
+
 
 //        for (Employee employee : employees) {
 //            System.out.println(employee.getName());
