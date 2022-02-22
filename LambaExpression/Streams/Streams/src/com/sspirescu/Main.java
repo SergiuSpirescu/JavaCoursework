94530a47f85c2b92c7555430d7905dda83a5e28e
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-02-22 21:06:28 +0200

    Created  new classes and flatMap implementation

--- LambaExpression/Streams/Streams/src/com/sspirescu/Main.java
+++ LambaExpression/Streams/Streams/src/com/sspirescu/Main.java
@@ -5,6 +5,7 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -38,10 +39,50 @@ public class Main {
                         .peek(System.out::println)
                         .count());
 
-        
+        Employee john = new Employee("John Doe", 30);
+        Employee jane = new Employee("Jane Deer", 25);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
 
+        Department hr = new Department("Human Resources");
+        hr.addEmployee(jane);
+        hr.addEmployee(jack);
+        hr.addEmployee(snow);
 
-//        someBingoNumbers.forEach(number -> {
+        Department accounting = new Department("Accounting");
+        accounting.addEmployee(john);
+
+        List<Department> departments = new ArrayList<>();
+        departments.add(hr);
+        departments.add(accounting);
+
+        departments.stream()
+                                .flatMap(department ->  department.getEmployees().stream())
+                .forEach(System.out::println);
+
+        System.out.println("---------------------------");
+//        List<String> sortedGNumbers = someBingoNumbers.stream()
+//                .map(String::toUpperCase)
+//                .filter(s -> s.startsWith("G"))
+//                .sorted()
+//                .collect(Collectors.toList());
+
+        List<String> sortedGNumbers = someBingoNumbers.stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+
+        for(String s: sortedGNumbers) {
+            System.out.println(s);
+    }
+
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .reduce((e1, e2) -> e1.getAge() < e2.getAge() ? e1 : e2)
+                .ifPresent(System.out::println);
+
+        //        someBingoNumbers.forEach(number -> {
 //            if (number.toUpperCase().startsWith("G")) {
 //                gNumbers.add(number);
 ////                System.out.println(number);
