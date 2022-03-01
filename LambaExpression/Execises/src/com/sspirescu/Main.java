a4b8ce1054c578b9ec99c051003d01cd45ae588c
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-03-01 18:12:49 +0200

    Challenge 1
    Created a Runnable object using Lambda Expression

--- LambaExpression/Execises/src/com/sspirescu/Main.java
+++ LambaExpression/Execises/src/com/sspirescu/Main.java
@@ -3,6 +3,27 @@ package com.sspirescu;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                String myString = "Split this into an array";
+                String[] parts = myString.split(" ");
+                for (String part: parts) {
+                    System.out.println(part + " ");
+                }
+            }
+        };
+
+        Runnable lambdaRunnable = () -> {
+            String myString = "Split this into an array";
+            String[] parts = myString.split(" ");
+            for (String part: parts) {
+                System.out.println(part + " ");
+            }
+        };
+
+//        lambdaRunnable.run();
+// Just to test it works
     }
 }
