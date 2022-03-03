2a0585c59d5f378c90d9348069bb51d8ee49939d
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-03-03 21:28:46 +0200

    RegExp Challenge 2

--- RegularExpression/RegularExpression1/src/com/sspirescu/Main.java
+++ RegularExpression/RegularExpression1/src/com/sspirescu/Main.java
@@ -10,8 +10,12 @@ public class Main {
     public static void main(String[] args) {
 
         String challenge1 = "I want a bike.";
+        String challenge2 = "I want a ball.";
 
-        System.out.println(challenge1.matches("I want a bike."));
+        System.out.println(challenge1.matches("I want a (bike|ball)."));
+        System.out.println(challenge2.matches("I want a (bike|ball)."));
+
+        System.out.println(challenge2.matches("I want a \\w+."));
 
 
     }
