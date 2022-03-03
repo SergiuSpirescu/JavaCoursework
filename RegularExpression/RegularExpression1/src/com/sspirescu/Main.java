ed91e43b17300eb5ebdfbaedc2241d88bcc35d66
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-03-03 20:54:25 +0200

    Video 4
    Using Matchers and GroupMatchers,
    matcher.reset to reset the matcher.
    
    Greedy quantifiers (*) = Grabs the maximum number of characters in a match
    vs.
    Lazy quantifiers (*? astetisk followed by question mark) = Grabs the minimum number of characters it can when searching for pattern
    
    Separating groups with ( )

--- RegularExpression/RegularExpression1/src/com/sspirescu/Main.java
+++ RegularExpression/RegularExpression1/src/com/sspirescu/Main.java
@@ -76,6 +76,8 @@ public class Main {
 
         System.out.println(thirdAlphanumericString.replaceAll("h+i*j", "Y"));
 
+        System.out.println("\n----------------------------\n");
+
         StringBuilder htmlText = new StringBuilder("<h1>My Heading</h1>");
         htmlText.append("<h2>Sub-Heading</h2>");
         htmlText.append("<p>Paragrah part 1</>");
@@ -83,12 +85,44 @@ public class Main {
         htmlText.append("<h2>Summary</h2>");
         htmlText.append("<p>Here is the summary.</p>");
 
-        String h2Pattern = ".*<h2>.*";
+        String h2Pattern = "<h2>";
 
         Pattern pattern = Pattern.compile(h2Pattern, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
 
         Matcher matcher = pattern.matcher(htmlText);
         System.out.println(matcher.matches());
+
+        matcher.reset();
+
+        int count = 0;
+
+        while(matcher.find()) {
+            count++;
+            System.out.println("Occurence " + count +": " + matcher.start() + " to " + matcher.end());
+        }
+
+        String h2GroupPatter = "(<h2>.*?</h2>)";
+        Pattern groupPattern = Pattern.compile(h2GroupPatter);
+        Matcher groupMatcher = groupPattern.matcher(htmlText);
+
+        System.out.println(groupMatcher.matches());
+        groupMatcher.reset();
+
+        while(groupMatcher.find()) {
+            System.out.println("Occurence " + groupMatcher.group(1));
+        }
+
+
+        String h2TextGroups = "(<h2>)(.+?)(</h2>)";
+        Pattern h2TextPattern = Pattern.compile(h2TextGroups);
+        Matcher h2TextMacher = h2TextPattern.matcher(htmlText);
+
+        while(h2TextMacher.find()) {
+            System.out.println("Occurence: " + h2TextMacher.group(2));
+        }
+
+
+
 
     }
 }
