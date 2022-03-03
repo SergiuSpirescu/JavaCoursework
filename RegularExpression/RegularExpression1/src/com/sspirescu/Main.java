15efdf447bab91ca4b18b4eb560c364f4448086f
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-03-03 21:37:53 +0200

    RegExp Challenge 5

--- RegularExpression/RegularExpression1/src/com/sspirescu/Main.java
+++ RegularExpression/RegularExpression1/src/com/sspirescu/Main.java
@@ -32,5 +32,13 @@ public class Main {
         System.out.println(challenge4.replaceAll("([ ])", "_"));
         System.out.println(challenge4.replaceAll("\\s", "_"));
 
+        String challenge5 = "aaabcccccccdddefffg";
+
+        String regExp5 = "^[a]{3}b{1}c{7}d{3}[e]{1}[f]{3}[g]{1}";
+
+        Pattern pattern2 = Pattern.compile(regExp5);
+        Matcher matcher2 = pattern2.matcher(challenge5);
+        System.out.println(matcher2.matches());
+
     }
 }
