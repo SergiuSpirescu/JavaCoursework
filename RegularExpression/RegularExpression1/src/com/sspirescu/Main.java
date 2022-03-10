a0754986caaafa9fb8e2aa4cab17aef1e6b90015
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-03-10 19:49:38 +0200

    Regular expressions (#10)
    * First commit of Regular Expression video
    
    Used a regular expression to modify a string
    
    * Used wildcard to replace characters in string
    
    Using a regular expression, modified all the characters using the wildcard character"."
    
    * Used the carrot wildcard
    
    Using the carrot wildcard (^), a specific subset of the string was modified with a RegExp.
    The carrot boundary matcher is used to match with the start of the string
    
    ^abcDeee was set to YYY
    
    * Added matches method
    
    The matches method returns True if the whole String equals the input parameter.
    
    * Dollar-sign matcher
    
    Matching strings that end with the matcher
    
    * Using the brackets [] marker
    
    The characters placed inside the brackets (here, they are a e and i) will be replaced with the input parameter.
    
    Here, all the a, e and i characters are replaced with X
    
    * Combining multiple brackets
    
    Using two different bracket matchers, the replaceAll method checks for a pattern where ANY of the characters in the first bracket are followed by ANY of the characters in the second bracket
    
    * Course 2, Part1
    
    Using carrot ^ inside square brackets -> it negates the search for the characters inside the brackets, they are excluded from search
    
    [a-f3-8] the dash character specifies the range a through f, and 3 through 8 respectively. spacing between the elements of the range is not necessary.
    
    (?i) -> special construct, to ignore case sensitivty
    
    * Course 2, Part 2
    
    "\\d" -> to search for all the numeric characters (digits)
    
    "\\D" -> to search for all the non-numeric characters
    
    "\\s" -> to remove whitespace characters
    
    "\\S" -> to remove non-whitespace characters
    
    "\\w" -> matches a-z, 0-9 and undescore (_)
    
    "\\b" -> matches the boundary of each word (position before and after)
    
    * Video 3
    
    Using Patterns, Matchers and StringBuilder
    .
    
    Using the pattern "<h2>" by itself is not working to find a match, but typing it as ".*<h2>.*" means that it is matching any character (via the dot .  matcher) for any number of occurences (via the asterisk * matcher)
    
    * Video 4
    
    Using Matchers and GroupMatchers,
    matcher.reset to reset the matcher.
    
    Greedy quantifiers (*) = Grabs the maximum number of characters in a match
    vs.
    Lazy quantifiers (*? astetisk followed by question mark) = Grabs the minimum number of characters it can when searching for pattern
    
    Separating groups with ( )
    
    * Video 5
    
    Using logical operations when writing regular expressions
    
    And -> it is implicit
    
    Or  -> use the pipe character |
    
    Not -> [^abc] carrot inside square brackets //
    (?!) --> negative look-ahead syntax
    
    also: (?=x) => positive look-ahead syntax - find all matches followed by x
    
    Using patterns to match only a correct phone number from a format and a visa card number
    
    * RegExp Challenge 1
    
    * RegExp Challenge 2
    
    * RegExp Challenge 3
    
    * RegExp Challenge 4
    
    * RegExp Challenge 5
    
    * RegExp Challenge 6
    
    * RegExp Challenge 7
    
    * RegExp Challenge 8
    
    Find all the digit groups in the string
    
    * RegExp Challenge 9
    
    Also including whitespace
    
    * RegExp Challenge 10
    
    Write indices for Start and End
    
    * RegExp Challenge 11
    
    Extracting coordinate data from an {x, y} container
    
    * RegExp Challenge 12
    
    Zip code prefix test
    
    * RegExp Challenge 13
    
    Adding - and 4 digits after first 5 digits
    
    * RegExp Challenge 14
    
    Using the pipe | in the regular expression

---
+++ RegularExpression/RegularExpression1/src/com/sspirescu/Main.java
@@ -0,0 +1,99 @@
+package com.sspirescu;
+
+import javafx.css.Match;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String challenge1 = "I want a bike.";
+        String challenge2 = "I want a ball.";
+
+        System.out.println(challenge1.matches("I want a (bike|ball)."));
+        System.out.println(challenge2.matches("I want a (bike|ball)."));
+
+        System.out.println(challenge2.matches("I want a \\w+."));
+
+        String regExp3 = "I want a \\w+.";
+        Pattern pattern = Pattern.compile(regExp3);
+        Matcher matcher = pattern.matcher(challenge1);
+
+        System.out.println(matcher.matches());
+
+        matcher = pattern.matcher(challenge2);
+        System.out.println(matcher.matches());
+
+
+        String challenge4 = "Replace all blanks with an underscore.";
+
+        System.out.println(challenge4.replaceAll("([ ])", "_"));
+        System.out.println(challenge4.replaceAll("\\s", "_"));
+
+        String challenge5 = "aaabcccccccdddefffg";
+
+//        String regExp5 = "^[a]{3}b{1}c{7}d{3}[e]{1}[f]{3}[g]{1}";
+        String regExp5 = "^a{3}bc{7}d{3}ef{3}g$";
+
+
+        Pattern pattern2 = Pattern.compile(regExp5);
+        Matcher matcher2 = pattern2.matcher(challenge5);
+        System.out.println(matcher2.matches());
+
+        System.out.println(challenge5.replaceAll(regExp5, "REPLACED"));
+
+        String challenge7 = "abcd.135uvqz.7tzik.999";
+
+//        String regExp7 = "^([a-z]){4}(\\.){1}([0-9]){3}$";
+        String regExp7 = ("[A-Za-z]+\\.(\\d+)");
+
+        Pattern pattern3 = Pattern.compile(regExp7);
+        Matcher matcher3 = pattern3.matcher(challenge7);
+
+        while(matcher3.find()) {
+            System.out.println("Some digits: " + matcher3.group(1));
+        }
+
+        String challenge9 = "abcd.135\tuvqz.7\ttzik.999\n";
+        Pattern pattern9 = Pattern.compile("[A-Za-z]+\\.(\\d+)\\s");
+        Matcher matcher9 = pattern9.matcher(challenge9);
+
+        while(matcher9.find()) {
+            System.out.println("Some digits: " + matcher9.group(1));
+        }
+
+        String challenge10 = "abcd.135\tuvqz.7\ttzik.999\n";
+        Pattern pattern10 = Pattern.compile("[A-Za-z]+\\.(\\d+)\\s");
+        Matcher matcher10 = pattern10.matcher(challenge10);
+
+        while(matcher10.find()) {
+            System.out.println("Start: " + matcher10.start(1) + " End: " + (matcher10.end(1) - 1));
+        }
+
+        String challenge11 = "{0, 2}, {0, 5}, {1, 3}, {2, 4}";
+
+        Pattern pattern11 = Pattern.compile("\\{(\\d{1}, \\d{1})\\}"); // My variant
+//        Pattern pattern11 = Pattern.compile("\\{(.+?)\\}"); // Tim's variant
+        Matcher matcher11 = pattern11.matcher(challenge11);
+
+        while (matcher11.find()) {
+            System.out.println("Coordinates: " + matcher11.group(1));
+        }
+
+        String challenge12 = "11111";
+
+        Pattern pattern12 = Pattern.compile("^\\d{5}$");
+        System.out.println(challenge12.matches("\\d{5}$"));
+
+        String challenge13 = "11111-1111";
+        System.out.println(challenge13.matches("^\\d{5}-\\d{4}$"));
+
+
+        System.out.println("--Challenge 14 results--\nString challenge12: "
+                +  challenge12.matches("^\\d{5}$|\\d{5}-\\d{4}$") +
+                "\nString challenge13: " + challenge13.matches("^\\d{5}$|\\d{5}-\\d{4}$"));
+
+    }
+}
