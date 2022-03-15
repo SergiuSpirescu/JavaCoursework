47bd362ee43c059dece16e2ba22e7642a9ea5f4a
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-03-15 21:37:13 +0200

    JUnit Challenge 1
    Create a Utilities class and a JUnit Test class for it

---
+++ JavaDebugging/src/com/sspirescu/UtilitiesTest.java
@@ -0,0 +1,37 @@
+package com.sspirescu;
+
+
+import static org.junit.Assert.*;
+
+public class UtilitiesTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        fail("No test implemented");
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+        fail("No test implemented");
+    }
+
+    @org.junit.Test
+    public void everyNthChar() {
+        fail("No test implemented");
+    }
+
+    @org.junit.Test
+    public void removePairs() {
+        fail("No test implemented");
+    }
+
+    @org.junit.Test
+    public void converter() {
+        fail("No test implemented");
+    }
+
+    @org.junit.Test
+    public void nullOddLength() {
+        fail("No test implemented");
+    }
+}
