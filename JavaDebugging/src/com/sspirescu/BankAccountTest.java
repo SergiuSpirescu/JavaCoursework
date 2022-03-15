bfa074f2191941c44c2f4c1dd4ccf48c644a2d5e
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-03-15 20:44:29 +0200

    Video 6 - Asserts
    Creating Unit Tests using the Assert functions

--- JavaDebugging/src/com/sspirescu/BankAccountTest.java
+++ JavaDebugging/src/com/sspirescu/BankAccountTest.java
@@ -6,16 +6,41 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() {
-        fail();
+        fail("This test has yet to be implemented");
     }
 
     @org.junit.Test
     public void doublewithdraw() {
-        fail();
+        BankAccount account = new BankAccount("Sergiu", "Tommy", 1000.00, BankAccount.CHECKING);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200, balance,0);
+
     }
 
     @org.junit.Test
-    public void getBalance() {
-        fail("O crepat..");
+    public void getBalance_deposit() {
+        BankAccount account = new BankAccount("Sergiu", "Tommy", 1000.00, BankAccount.CHECKING);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
     }
+
+    @org.junit.Test
+    public void getBalance_withdraw() {
+        BankAccount account = new BankAccount("Sergiu", "Tommy", 1000.00, BankAccount.CHECKING);
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void isChecking_True() throws Exception {
+        BankAccount account = new BankAccount("Sergiu", "Tommy", 1000.00, BankAccount.CHECKING);
+        assertTrue("The account is NOT a Checking account", account.isChecking());
+    }
+
+//    @org.junit.Test
+//    public void dummyTest() {
+//        assertEquals(20, 21);
+//    }
+
+
 }
