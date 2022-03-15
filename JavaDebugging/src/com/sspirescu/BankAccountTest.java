45b7e27fd482393c42135077a1553b73d7bbaf42
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-03-15 20:23:51 +0200

    Video 5 - Unit Testing
    Using JUnit to create unit tests

---
+++ JavaDebugging/src/com/sspirescu/BankAccountTest.java
@@ -0,0 +1,21 @@
+package com.sspirescu;
+
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+    @org.junit.Test
+    public void deposit() {
+        fail();
+    }
+
+    @org.junit.Test
+    public void doublewithdraw() {
+        fail();
+    }
+
+    @org.junit.Test
+    public void getBalance() {
+        fail("O crepat..");
+    }
+}
--- JavaDebugging/src/com/sspirescu/Main.java
+++ JavaDebugging/src/com/sspirescu/Main.java
@@ -3,19 +3,8 @@ package com.sspirescu;
 public class Main {
 
     public static void main(String[] args) {
-        StringUtilities utils = new StringUtilities();
 
-        StringBuilder sb = new StringBuilder();
-
-        while (sb.length() < 10) {
-            utils.addChar(sb, 'a');
-        }
-        System.out.println(sb);
-
-        String str = "abcdefg";
-        String result = utils.upperAndPrefix(utils.addSuffix(str));
-
-
+        System.out.println("The app is running just fine!");
     }
 
 }
