30f3658e4ddc1b0a706992a3e64bd5a9b99326ef
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-03-15 21:52:23 +0200

    JUnit Challenge 2
    Created tests for the method removePairs and solved bugs

--- JavaDebugging/src/com/sspirescu/Main.java
+++ JavaDebugging/src/com/sspirescu/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("The app is running just fine!");
+
+        Utilities util = new Utilities();
+
+        util.removePairs("AABCDDEFF");
     }
 
 }
--- JavaDebugging/src/com/sspirescu/UtilitiesTest.java
+++ JavaDebugging/src/com/sspirescu/UtilitiesTest.java
@@ -5,15 +5,15 @@ import static org.junit.Assert.*;
 
 public class UtilitiesTest {
 
-    @org.junit.Before
-    public void setUp() throws Exception {
-        fail("No test implemented");
-    }
+//    @org.junit.Before
+//    public void setUp() throws Exception {
+//        fail("No test implemented");
+//    }
 
-    @org.junit.After
-    public void tearDown() throws Exception {
-        fail("No test implemented");
-    }
+//    @org.junit.After
+//    public void tearDown() throws Exception {
+//        fail("No test implemented");
+//    }
 
     @org.junit.Test
     public void everyNthChar() {
@@ -33,5 +33,19 @@ public class UtilitiesTest {
     @org.junit.Test
     public void nullOddLength() {
         fail("No test implemented");
+    }
+
+    @org.junit.Test
+    public void removePairs_test1() throws Exception {
+        Utilities utilities = new Utilities();
+        assertEquals("ABCDEF", utilities.removePairs("AABCDDEFF"));
+        System.out.println("Test PASSED");
+    }
+
+    @org.junit.Test
+    public void removePairs_test2() throws Exception {
+        Utilities utilities = new Utilities();
+        assertEquals("ABCABDEF", utilities.removePairs("ABCCABDEEF"));
+        System.out.println("Test PASSED");
     }
 }
