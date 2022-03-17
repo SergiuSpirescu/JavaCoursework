70a26098ad1c11f217ad7e780f3914b34f8ee5f5
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-03-17 20:57:59 +0200

    JUnit Challenge 9
    Created a unique variable to store the instance and initialize it Before each test, using a mehtod with the @org.junit.Before annotation

--- JavaDebugging/src/com/sspirescu/UtilitiesTest.java
+++ JavaDebugging/src/com/sspirescu/UtilitiesTest.java
@@ -1,69 +1,53 @@
 package com.sspirescu;
 
 
+import org.junit.Test;
+
 import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class UtilitiesTest {
 
-//    @org.junit.Before
-//    public void setUp() throws Exception {
-//        fail("No test implemented");
-//    }
+    private Utilities util;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+       util = new Utilities() ;
+    }
 
 //    @org.junit.After
 //    public void tearDown() throws Exception {
 //        fail("No test implemented");
 //    }
 
-    @org.junit.Test
-    public void everyNthChar() {
-        fail("No test implemented");
-    }
-
-    @org.junit.Test
-    public void removePairs() {
-        fail("No test implemented");
-    }
-
-    @org.junit.Test
-    public void converter() {
-        fail("No test implemented");
-    }
-
-    @org.junit.Test
-    public void nullOddLength() {
-        fail("No test implemented");
-    }
-
-    @org.junit.Test
+    @Test
     public void removePairs_test1() throws Exception {
-        Utilities utilities = new Utilities();
-        assertEquals("ABCDEF", utilities.removePairs("AABCDDEFF"));
+//        Utilities utilities = new Utilities();
+        assertEquals("ABCDEF", util.removePairs("AABCDDEFF"));
         System.out.println("Test PASSED");
-        assertNull("Did not get null returned when argument passed was null", utilities.removePairs(null));
-        assertEquals("A", utilities.removePairs("A"));
-        assertEquals("", utilities.removePairs(""));
+        assertNull("Did not get null returned when argument passed was null", util.removePairs(null));
+        assertEquals("A", util.removePairs("A"));
+        assertEquals("", util.removePairs(""));
     }
 
     @org.junit.Test
     public void removePairs_test2() throws Exception {
-        Utilities utilities = new Utilities();
-        assertEquals("ABCABDEF", utilities.removePairs("ABCCABDEEF"));
+//        Utilities utilities = new Utilities();
+        assertEquals("ABCABDEF", util.removePairs("ABCCABDEEF"));
         System.out.println("Test PASSED");
     }
 
     @org.junit.Test
     public void everyNthChar_test1() throws Exception {
-        Utilities util = new Utilities();
+//        Utilities util = new Utilities();
         char[] output = util.everyNthChar(new char[] {'h', 'e', 'l', 'l', 'o'},2);
         assertArrayEquals(new char[] {'e','l'}, output);
     }
 
     @org.junit.Test
     public void everyNthChar_test2() throws Exception {
-        Utilities util = new Utilities();
+//        Utilities util = new Utilities();
         char[] output = util.everyNthChar(new char[] {'h', 'e', 'l', 'l', 'o'},8);
         assertArrayEquals(new char[] {'h', 'e', 'l', 'l', 'o'}, output);
     }
