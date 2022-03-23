1bd0ca941f708170516493a43d587b7672a1a0f9
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-03-23 20:35:24 +0200

    [Video 15] Adding more records with/o autocommit
    Stopped the auto-commit to see how it works.
    
    Reapplied it and inserted more records.
    
    Updated a record.

--- Databases/src/com/sspirescu/Main.java
+++ Databases/src/com/sspirescu/Main.java
@@ -11,12 +11,21 @@ public class Main {
 
         try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\SS-PC\\Ciorne\\JavaCoursework\\Databases\\testjava.db");
+//            conn.setAutoCommit(false);
             Statement statement = conn.createStatement();
-            statement.execute("CREATE TABLE IF NOT EXISTS Contacts" +
-                    " (name TEXT, phone INTEGER, email TEXT)");
+//            statement.execute("CREATE TABLE IF NOT EXISTS Contacts" +
+////                    " (name TEXT, phone INTEGER, email TEXT)");
+////
+////            statement.execute("INSERT INTO Contacts (name,phone,email)" +
+////                    "VALUES ('Joe',55555, 'joe@myemail.com')");
+////
+////            statement.execute("INSERT INTO Contacts (name,phone,email)" +
+////                    "VALUES ('Jane',429484, 'jane@somewhere.com')");
+////
+////            statement.execute("INSERT INTO Contacts (name,phone,email)" +
+////                    "VALUES ('Fido',9038, 'dog@email.com')");
 
-            statement.execute("INSERT INTO Contacts (name,phone,email)" +
-                    "VALUES ('Tim',654321, 'tim@email.com')");
+            statement.execute("UPDATE Contacts SET phone=5566789 WHERE name='Jane'");
 
             statement.close();
             conn.close();
