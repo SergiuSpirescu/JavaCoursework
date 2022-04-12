3e6cc20c014df6aa4d64c662dc21a69e46231f94
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-04-12 21:02:50 +0300

    Updated Artist class
    Updated Artist class with new property types

--- Databases/GUIDatabase/src/sample/Controller.java
+++ Databases/GUIDatabase/src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import sample.model.Artist;
+import sample.model.DataSource;
+
 public class Controller {
+}
+
+class GetAllArtistsTask extends Task{
+    @Override
+    public ObservableList<Artist> call() throws Exception {
+        return FXCollections.observableArrayList(
+                DataSource.getInstance().queryArtists(DataSource.ORDER_BY_ASC)
+        );
+    }
 }
