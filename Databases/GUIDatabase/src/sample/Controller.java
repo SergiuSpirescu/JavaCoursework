0ed417e70fa2e13d91b85b741627e97d9f7e38d1
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-04-12 21:14:22 +0300

    Added methods to bind View and Controller
    Created a listArtists method to bind the Artist query to a TableView

--- Databases/GUIDatabase/src/sample/Controller.java
+++ Databases/GUIDatabase/src/sample/Controller.java
@@ -3,13 +3,21 @@ package sample;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import sample.model.Artist;
 import sample.model.DataSource;
 
-import javax.xml.crypto.Data;
 
 public class Controller {
 
+    @FXML
+    private TableView<Artist> artistTable;
+
+    public void listArtists() {
+        Task<ObservableList<Artist>> task = new GetAllArtistsTask();
+        artistTable.itemsProperty().bind(task.valueProperty());
+    }
 
 }
 
