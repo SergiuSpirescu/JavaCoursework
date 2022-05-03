94fe23b1faa591eca516d12cf98bc564ac18edc1
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-05-03 21:05:42 +0300

    Video 3 - MultiThreaded Server
    Created a new class Echoer to help implement multithreading in the server side

--- Networking/EchoServer/src/com/sspirescu/Main.java
+++ Networking/EchoServer/src/com/sspirescu/Main.java
@@ -1,11 +1,8 @@
 package com.sspirescu;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
+
 
 public class Main {
 
@@ -13,21 +10,8 @@ public class Main {
 
         try(ServerSocket serverSocket = new ServerSocket(5000)) {
 
-            Socket socket = serverSocket.accept();
-            System.out.println("Client connected");
-
-            BufferedReader input = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream())
-            );
-
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
             while (true) {
-                String echoString = input.readLine();
-                if(echoString.equals("exit")) {
-                    break;
-                }
-                output.println("Echo From Server: " + echoString);
+               new Echoer(serverSocket.accept()).start();
             }
 
         } catch (IOException e) {
